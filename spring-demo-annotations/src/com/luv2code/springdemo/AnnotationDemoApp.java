08668acab18feb4a82d48d2fad3aa1cb051f374a
kalyaniblv <prasadbv580@gmail.com>
2021-02-19 19:01:09 +0530

    mm

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -3,7 +3,7 @@ package com.luv2code.springdemo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationDemoApp {
-
+//
 	public static void main(String[] args) {
 		// Read spring config file
 		ClassPathXmlApplicationContext c = new ClassPathXmlApplicationContext("AnnotationDemo-applicationContext.xml");
